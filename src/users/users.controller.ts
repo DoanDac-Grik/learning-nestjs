dc96c71e2533e6c15a1c7c751e6c2203dfc5463d
DoanDac-Grik <dac.doan@positivethinking.tech>
2022-08-21 23:22:41 +0700

    an example to use session cookie, 2 apis to set and get color

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -10,6 +10,16 @@ import { UsersService } from './users.service';
 export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService) {}
     
+    @Get('/colors/:color')
+    setColor(@Param('color') color: string, @Session() session: any) {
+        session.color = color;
+    }
+
+    @Get('/colors/')
+    getColor(@Session() session: any){
+        return session.color;
+    }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         return this.authService.signUp(body.email, body.password);
