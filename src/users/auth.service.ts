70c707c9afec943500ff73443ee33e76dc579b50
DoanDac-Grik <dac.doan@positivethinking.tech>
2022-08-21 23:03:48 +0700

    hash salt password, handle signin signup func

---
+++ src/users/auth.service.ts
@@ -0,0 +1,44 @@
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+import { UsersService } from "./users.service";
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+    constructor(private userService: UsersService) {}
+
+    async signUp(email: string, password: string){
+        const checkUser = this.userService.find(email);
+        if(checkUser){
+            throw new BadRequestException(`User ${email} in use`);
+        }
+
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
+        
+        const user = await this.userService.create(email, result);
+
+        return user;
+        
+    }
+
+    async signIn(email: string, password: string){
+        const [user] = await this.userService.find(email);
+        if(!user){
+            throw new NotFoundException(`User ${email} not found`);    
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+        
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if(hash.toString('hex') !== storedHash){
+            throw new BadRequestException('Wrong password');          
+        }
+
+        return user;
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -7,11 +8,16 @@ import { UsersService } from './users.service';
 @Controller('users')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) {   }
+    constructor(private usersService: UsersService, private authService: AuthService) {}
     
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+        return this.authService.signUp(body.email, body.password);
+    }
+
+    @Post('/signin')
+    signIn(@Body() body: CreateUserDto) {
+        return this.authService.signIn(body.email, body.password);
     }
 
     @Get('/:id')
